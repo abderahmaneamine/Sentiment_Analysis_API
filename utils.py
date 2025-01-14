15b05d67902e5a3e9cad64d41da07dc8f5ef69cb
Abderahmane Amine Boudelal <127012091+abderahmaneamine@users.noreply.github.com>
2025-01-14 11:12:54 +0100

    Delete utils.py

--- utils.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline
-nlp = pipeline("sentiment-analysis", model="finiteautomata/bertweet-base-sentiment-analysis")
-def generate(text: str):
-    sentiment = nlp(text)
-    return sentiment
